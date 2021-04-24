de57ebca1e68d017993468e0be6292decf2a1532
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2021-04-24 17:42:20 -0400

    Update start.py
    Adding buttons

--- start.py
+++ start.py
@@ -1,5 +1,19 @@
 import os
+from guizero import App, Text, TextBox, PushButton
 
+def do_nothing():
+    #Text.text = "Button was pressed"
+    text.destroy()
+
+
+app = App(width = 500, height = 200)
+text = Text(app, text="Paste Video URL below")
+input_box = TextBox(app, width = 600)
+button_dest = PushButton(app, text = "Select Save Directory", command=do_nothing)
+button_start = PushButton(app, command=do_nothing)
+app.display()
+
+'''
 while True:
     
     url = input("Paste URL\n")
@@ -8,3 +22,4 @@ while True:
 
     os.system("youtube-dl {}".format(url))
     #os.system("cmd /k echo hello world")
+'''
