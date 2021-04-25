f6ac38de23040bd374587cdee6d0c961ed8bf635
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2021-04-25 18:45:33 -0400

    Program Working
    Standard download working. Need to fix output folder duplication error. Try to include progress bar

--- start.py
+++ start.py
@@ -7,26 +7,30 @@ import youtube_dl
 import subprocess as sp
 import time
 #Tk().withdraw()
-'''
+
 save = ""
 
 def save_dir():
+    global save
     #Text.text = "Button was pressed"
-    save = askdirectory()
-    print(save)
+    folder = askdirectory()
+    save += folder
     save_text = Text(app, text="Saving to..." + save )
     button_start = PushButton(app, text="Start Download", command=download)
 
 
 def download():
-    global save
+    #global save
+    #print(save)
+    #input()
     #Text.text = "Button was pressed"
     
     ydl_opts = {
-    'outtmpl': '{}/%(title)s.%(ext)s'.format(save)
+    'outtmpl': '{}\%(title)s.%(ext)s'.format(save)
     }
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download(['{}'.format(input_box.value)])
+    print(ydl_opts)
     
 
 
@@ -41,17 +45,21 @@ button_dest = PushButton(app, width = 50, text = "Chose Save Location", command=
 app.display()
 
 #output = "C:\\Users\\Office\\Documents\\EGDownloads\\Media"
+
 '''
 url = input()
 out = sp.getoutput('youtube-dl --get-filename https://www.youtube.com/watch?v=BzUV9BciHe8')
 to = "C:\\Users\\ATUser\\Desktop\\mp3"
-fr = "{}.mp4".format(out)
+fr = "{}".format(out)
+print(fr)
 os.system('cmd /c "youtube-dl {}'.format(url))
 time.sleep(2)
 os.system('cmd /c "move {} {}'.format(fr, to))
+'''
+
+
 
 
 #print(out)
-
 
 
