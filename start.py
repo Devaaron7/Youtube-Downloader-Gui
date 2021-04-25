fd95d7e4869921c10ae8810094e43fd6367961d4
Devaaron7 <65022882+Devaaron7@users.noreply.github.com>
2021-04-25 08:52:15 -0400

    Update start.py

--- start.py
+++ start.py
@@ -1,25 +1,41 @@
+from __future__ import unicode_literals
 import os
 from guizero import App, Text, TextBox, PushButton
+from tkinter import Tk
+from tkinter.filedialog import askopenfilenames, askdirectory
+import youtube_dl
+#Tk().withdraw()
 
-def do_nothing():
+
+def download():
     #Text.text = "Button was pressed"
-    text.destroy()
+    ydl_opts = {}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(['{}'.format(input_box.value)])
+
+
+def save_dir():
+    #Text.text = "Button was pressed"
+    save = askdirectory()
+    print(save)
+    save_text = Text(app, text="Saving to..." + save )
+    button_start = PushButton(app, text="Start Download", command=download)
 
 
 app = App(width = 500, height = 200)
-text = Text(app, text="Paste Video URL below")
+
+url_text = Text(app, text="Paste Video URL below")
+
 input_box = TextBox(app, width = 600)
-button_dest = PushButton(app, text = "Select Save Directory", command=do_nothing)
-button_start = PushButton(app, command=do_nothing)
+
+button_dest = PushButton(app, width = 50, text = "Chose Save Location", command=save_dir)
+
 app.display()
 
-'''
-while True:
-    
-    url = input("Paste URL\n")
+#output = "C:\\Users\\Office\\Documents\\EGDownloads\\Media"
 
-    #output = "C:\\Users\\Office\\Documents\\EGDownloads\\Media"
 
-    os.system("youtube-dl {}".format(url))
-    #os.system("cmd /k echo hello world")
-'''
+
+
+
+
